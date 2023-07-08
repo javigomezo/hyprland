b87254eb8309bda09f06c59ce80bd85ea5121f6b
javigomezo <javiergomezo@protonmail.com>
2023-07-08 16:37:19 +0200

    feat(wezterm): add basic wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Set Nord color scheme
+config.color_scheme = 'nord'
+
+-- Disable Tab Bar
+config.enable_tab_bar = false
+
+-- Fonts
+config.font = wezterm.font_with_fallback {
+  'Comic Code Ligatures',
+  'Noto Color Emoji',
+  'CartographCF Nerd Font'
+}
+
+-- and finally, return the configuration to wezterm
+return config
+
